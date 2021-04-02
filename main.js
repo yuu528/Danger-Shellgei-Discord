3cdd76c1be5b8f457aa2ae117985c3e190a20a96
yuu528 <yuu528mail@gmail.com>
2021-04-02 12:11:44 +0900

    Improve code

--- main.js
+++ main.js
@@ -12,14 +12,25 @@ client.on('ready', () => {
 
 client.on('message', msg => {
 	let mentionRegEx = new RegExp('<@!?' + client.user.id + '>');
+
+	//bot以外からのメンションなら
 	if(!msg.author.bot && mentionRegEx.test(msg.content)) {
+		//一時スクリプトを置くパスをユーザーIDと時刻から決定
 		let path = __dirname + '/tmp/' + msg.author.id + msg.createdTimestamp;
+
+		//メンションを消して空行と前後の空白を取るため二度trim()
 		let writeCmd = msg.content.replace(mentionRegEx, '').trim().trim();
+
+		//Shebangがなければbashに
 		if(!/^#!/.test(writeCmd)) {
 			writeCmd = '#!/bin/bash\n' + writeCmd;
 		}
+
+		//一時ディレクトリ・スクリプトの作成
 		fs.mkdirSync(path);
 		fs.writeFileSync(path + '/run_tmp', writeCmd, {mode: 0o777});
+
+		//30secの制限でスクリプトを実行
 		child.exec(path + '/run_tmp', {timeout: 30000, cwd: path}, (err, stdout, stderr) => {
 			if(err != null) {
 				msg.channel.send('**Error**\n```' + stderr +'```');
@@ -27,6 +38,7 @@ client.on('message', msg => {
 				msg.channel.send(stdout);
 			}
 
+			//一時ディレクトリ削除
 			fs.rmdir(path, {recursive: true}, err => {
 				if(err != null) throw err;
 			});
